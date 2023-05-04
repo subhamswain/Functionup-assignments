700a0a3b9ab064270b8a39f8f5210280b0f94d04
shailja026 <shailjagupta4466@gmail.com>
2023-05-05 02:53:25 +0530

    done

---
+++ weak3/day4/dogImg/img/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {DogImgs} from "./component/DogImg.jsx"
+
+function App() {
+  return (
+    <div className="App">
+     <DogImgs/>
+    </div>
+  );
+}
+
+export default App;
