f38f94aa7551240fea9883dd31ee274a61ed464d
shailja026 <shailjagupta4466@gmail.com>
2023-05-05 23:38:14 +0530

    done

--- weak3/day4/otpGenerator/otp/src/component/Otp.jsx
+++ weak3/day4/otpGenerator/otp/src/component/Otp.jsx
@@ -1,44 +1,39 @@
 import axios from "axios"
 import { useState } from "react"
 
-export function OtpGenerator(){
- const [number ,setNumber] = useState("")
- //const [mobile,setMobile] = useState("")
- 
-let url = "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP"
-    function LetsGenerateOtp(){
-        axios({
-            method: 'post',
-            url: url,
-            data: {
-                mobile: number
-            }
-        }) 
-       
-        .then((res)=>res
-           )
-           
+export function OtpGenerator() {
+    const [number, setNumber] = useState("")
+    //const [mobile,setMobile] = useState("")
+
+    let url = "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP"
+    function LetsGenerateOtp() {
+        axios.post(url, { mobile: Number(number) })
+            .then((res) => { console.log(res) })
+            .catch((error) => { console.log(error) })
     }
-    function readTheInput(e){
-        if(number.length<10){
+    function readTheInput(e) {
+        if (number.length <= 10) {
             setNumber(e.target.value)
             console.log(number)
-        }else{
-            alert("enter a valid number")
         }
-     
-    }
-  return(
-       <div>
-       <input
-     type="number"
-     
-     value={number}
-      onChange={readTheInput} 
-       />
-       <button onClick={LetsGenerateOtp}>Get Otp</button>
 
-       </div> 
+    }
+    function newFunc() {
+        if (number.length !== 10) {
+            alert("enter valid number")
+        } else {
+            LetsGenerateOtp()
+        }
+
+    }
+    return (
+        <div>
+            <input
+                type="number"
+                value={number}
+                onChange={readTheInput} />
+            <button onClick={newFunc}>Get Otp</button>
+        </div>
     )
 
 }
