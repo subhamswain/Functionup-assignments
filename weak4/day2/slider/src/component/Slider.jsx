0121d1e2995d5343d46087ed15e4d9514c6f53b6
shailja026 <shailjagupta4466@gmail.com>
2023-05-11 00:37:53 +0530

    slider done

---
+++ weak4/day2/slider/src/component/Slider.jsx
@@ -0,0 +1,20 @@
+import styles from "./Slider.module.css"
+
+export const Slider = ({elements}) => {
+
+
+    return(
+        <div className={styles.MainContainer}>
+        
+        <img src={elements.avatar} alt=""/>
+
+       <h1>{elements.id}</h1>
+        <h2>{elements.email}</h2>
+        <h1>{elements.first_name}  {elements.last_name}</h1>
+        
+       
+       
+         
+        </div>
+    )
+}
