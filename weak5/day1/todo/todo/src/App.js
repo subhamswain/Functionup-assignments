a1e60495d6d5c46b7b4c30e132ff08b0336a37c3
shailja026 <shailjagupta4466@gmail.com>
2023-05-17 12:46:42 +0530

    got required output

--- weak5/day1/todo/todo/src/App.js
+++ weak5/day1/todo/todo/src/App.js
@@ -6,31 +6,27 @@ import { useState } from 'react';
 function App() {
   let [arr, setArr] = useState(["Shailja", "Deepak", "Komal", "Harshit"])
 
-  function DeleteItem({a}) {
-    // const newArr = [...arr]
-    // newArr.slice(a,1);
-    // setArr(newArr)
-    // let i = arr.indexOf(a)
-    // const newArr = arr.filter((el,index)=> index !== i);
-    // setArr(newArr)
-    // setArr([...arr.slice(0,i),...arr.slice(i+1)])
-    const nayiArr = arr.filter((el,ind) => 
-     a!==ind
-    );
-    setArr([...nayiArr])
+  function DeleteItem(index) {
+ 
+  let newArr = arr.filter((el,id)=> {
+    return id!==index
+  })
+  setArr(newArr)
   }
   function AddingElement(data) {
+    if(data!==""){
     const newArr = [...arr]
     newArr.push(data);
     setArr([...newArr])
+    }
   }
 
   return (
     <div className="App">
       {arr.map((el, index) => {
-        return <div style={{ display: "flex", justifyContent: "space-between", width: "30vw" }} a= {index}>
+        return <div  a= {index}>
           <h1>{el}</h1>
-          <button onClick={DeleteItem}  >Delete</button>
+          <button onClick={()=>{DeleteItem(index)}}  >Delete</button>
          </div>
       })}
        <ToDo onAdding={AddingElement} />
--- weak5/day1/todo/todo/src/component/todo.jsx
+++ weak5/day1/todo/todo/src/component/todo.jsx
@@ -1,4 +1,4 @@
- 
+ import "./todo.css"
  import { useState } from "react"
 export const ToDo = ({onAdding}) => {
  const [value , setValue] = useState("")
@@ -6,16 +6,18 @@ export const ToDo = ({onAdding}) => {
     onAdding(value)
  }
     return (
-        <>
+        <div className="main" style={{backgroundColor:"rgb(175, 107, 107)",padding:"10px"}}>
         <input
+        className="input"
          type="text"
          value={value}
          onChange={(e) => {
             setValue(e.target.value)
          }}
+         placeholder="Enter your value"
 
         />
         <button onClick={addSome}>+</button>
-        </>
+        </div>
     )
 }
